7282fac299407011dbb59f01cc98cb715ad2824d
Evulpes <54101182+Evulpes@users.noreply.github.com>
2020-05-28 19:05:12 +0100

    Add project files.

---
+++ ByteScan.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Linq;
+
+namespace Generic_Bytescan_Library
+{
+    public class ByteScan : NativeMethods
+    {
+        public static int FindByteSequence(Process process, byte[] pattern, out IntPtr[] offsets, bool findAll = false)
+        {
+            List<IntPtr> offsetList = new List<IntPtr>();
+            offsets = new IntPtr[] { (IntPtr)0 };
+
+            //Create a handle to the process
+            IntPtr processHandle = Processthreadsapi.OpenProcess(0x0010 | 0x0020 | 0x0008, false, process.Id);
+
+            //Initialize the a modInfo struct as new so the size can be safely obtained
+            var modInfo = new Psapi.ModuleInfo();
+
+            //Allocated some memory for a ptr
+            IntPtr modInfoPtr = Marshal.AllocHGlobal(Marshal.SizeOf(modInfo));
+
+            //Get the module info for the process base
+            bool ntStatus = Psapi.GetModuleInformation(processHandle, process.MainModule.BaseAddress, modInfoPtr, (uint)Marshal.SizeOf(modInfo));
+            if (!ntStatus)
+                return Marshal.GetLastWin32Error();
+
+            //Convert the ptr to the structure
+            modInfo = (Psapi.ModuleInfo)Marshal.PtrToStructure(modInfoPtr, typeof(Psapi.ModuleInfo));
+
+            //Allocate a new buffer based on the size of the image
+            byte[] lpBuffer = new byte[modInfo.SizeOfImage];
+
+            //Read the entire image into the buffer
+            ntStatus = Memoryapi.ReadProcessMemory(processHandle, process.MainModule.BaseAddress, lpBuffer, (int)modInfo.SizeOfImage, out IntPtr _);
+            if (!ntStatus)
+                return Marshal.GetLastWin32Error();
+
+            for (int i = 0; i < lpBuffer.Length; i++)
+            {
+                //Create a new array to copy potential matches to
+                byte[] tempArray = new byte[pattern.Length];
+
+                if (lpBuffer[i] == pattern[0])
+                {
+                    for (int x = 0; x < pattern.Length; x++)
+                        tempArray[x] = lpBuffer[i + x];
+
+                    if(Enumerable.SequenceEqual(tempArray, pattern))
+                    {
+                        //Add the index of the byte[] to  the offset list
+                        offsetList.Add((IntPtr)i);
+
+                        if (!findAll)
+                            break;
+                    }
+
+                }
+            }
+            offsets = offsetList.ToArray();
+            Handleapi.CloseHandle(processHandle);
+            Marshal.FreeHGlobal(modInfoPtr);
+            return 0;
+        }
+    }
+}
---
+++ NativeMethods.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Generic_Bytescan_Library
+{
+    public class NativeMethods
+    {
+        public static class Handleapi
+        {
+            [DllImport("Kernel32")]
+            public static extern bool CloseHandle(IntPtr hObject);
+        }
+
+        public static class Memoryapi
+        {
+            [DllImport("kernel32.dll", SetLastError = true)]
+            public static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, [Out] byte[] lpBuffer, int dwSize, out IntPtr lpNumberOfBytesRead);
+
+            [DllImport("kernel32.dll", SetLastError = true)]
+            public static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, int nSize, out IntPtr lpNumberOfBytesWritten);
+        }
+        public static class Processthreadsapi
+        {
+            [DllImport("Kernel32", SetLastError = true)]
+            public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int processId);
+        }
+        public static class Psapi
+        {
+            [DllImport("psapi", SetLastError = true)]
+            public static extern bool GetModuleInformation(IntPtr hProcess, IntPtr hModule, IntPtr lpmodinfo, uint cb);
+
+            public struct ModuleInfo
+            {
+                public IntPtr lpBaseOfDll;
+                public uint SizeOfImage;
+                public IntPtr EntryPoint;
+            }
+        }
+    }
+}
